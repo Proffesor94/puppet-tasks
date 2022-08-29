0312d6e2f6d042d6370698a8d2343fe1ef84f39f
Pavel Shumilin <serfer94@gmail.com>
2022-08-29 16:00:15 +0300

    Rewritten to use modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,72 +1,19 @@
 node 'slave1.puppet' {
 
- package { 'httpd':
-  ensure => latest,
- }
-
- file {'/var/www/html/index.html':
-  ensure => file,
-  source => 'puppet:///modules/static/index.html'
- }
-
- service { 'httpd':
-  ensure => running,
-  enable => true,
- }
-
- service { 'firewalld':
-  ensure => stopped,
-  enable => false,
- }
-
- file {'/root/README':
-  ensure => absent,
- }
+  include static
 }
 
 node 'slave2.puppet' {
 
- package { ['httpd','php'] :
-  ensure => latest,
- }
-
- file {'/var/www/html/index.php':
-  ensure => file,
-  source => 'puppet:///modules/dynamic/index.php'
- }
- 
- service { 'httpd':
-  ensure => running,
-  enable => true,
- }
- 
- service { 'firewalld':
-  ensure => stopped,
-  enable => false,
- }
- 
- file {'/root/README':
-  ensure => absent,
- }
+  include dynamic
 }
 
 node 'master.puppet' {
 
-include nginx
-
-nginx::resource::server { 'proxy-static':
-    listen_port => 80,
-    proxy       => 'http://192.168.56.21:80',
-                        }
-
-nginx::resource::server { 'proxy-dynamic':
-    listen_port => 81,
-    proxy       => 'http://192.168.56.22:80',
-                        }
+  include proxy
 }
 
 node mineserver.puppet {
 
-  include role::mineserver
-  
+  include role::mineserver  
 }
---
+++ modules/master_proxy/manifests/init.pp
@@ -0,0 +1,14 @@
+class proxy {
+ 
+include nginx
+
+nginx::resource::server { 'proxy-static':
+    listen_port => 80,
+    proxy       => 'http://192.168.56.21:80',
+                        }
+
+nginx::resource::server { 'proxy-dynamic':
+    listen_port => 81,
+    proxy       => 'http://192.168.56.22:80',
+                        }
+}
